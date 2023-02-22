2c113dc5c5b423c4ba9e4271aa85a04ce37c630a
fakhryfernanda <mff023@gmail.com>
2023-02-22 22:53:35 +0700

    (+) theme extend (+) line clamp plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,25 @@ module.exports = {
 
             },
             spacing: {
+                44: '11rem',
                 70: '17.5rem',
                 175: '43.75rem',
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
             },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
+            },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };

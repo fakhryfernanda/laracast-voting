a7773e6cfdd5843234aef3a5c2eeed633e3d2e60
fakhryfernanda <mff023@gmail.com>
2023-02-22 23:27:36 +0700

    (+) spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
 
             },
             spacing: {
+                22: '5.5rem',
                 44: '11rem',
                 70: '17.5rem',
                 175: '43.75rem',

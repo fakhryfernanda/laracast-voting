21f9642d3984bdeb345cb378347d4d2019dac167
fakhryfernanda <mff023@gmail.com>
2023-02-22 22:56:34 +0700

    (+) add idea card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,55 @@
 
         <main class="container mx-auto max-w-[1200px] flex">
             <div class="max-w-[280px] mr-5">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis nam assumenda voluptatem doloremque vero explicabo velit maxime! Praesentium harum et exercitationem, dolorum itaque maiores placeat illo esse! Eum, numquam vitae et sed consequuntur blanditiis dolore asperiores atque animi quo nisi. Dolor, minus? Quae molestias quisquam expedita fugit animi ipsam ipsum, iusto nemo? Corporis explicabo, excepturi, libero earum fuga omnis cum facere voluptatum ut perspiciatis nihil commodi aspernatur soluta? Fugit illo sapiente adipisci quo nostrum minima nam officia officiis voluptates iste, sed dolor laudantium hic ullam maxime quaerat a. Placeat, officia. Neque deserunt in unde, assumenda illum eius a? Nostrum, vero.
+                <div
+                    class="bg-white border-2 border-blue rounded-xl mt-16"
+                    style="
+                          border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                            border-image-slice: 1;
+                            background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                            background-origin: border-box;
+                            background-clip: content-box, border-box;
+                    "
+                >
+                    <div class="text-center px-6 py-2 pt-6">
+                        <h3 class="font-semibold text-base">Add an idea</h3>
+                        <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                    </div>
+
+                    <form action="#" method="POST" class="space-y-4 px-4 py-6">
+                        <div>
+                            <input type="text" class="w-full text-sm bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
+                        </div>
+                        <div>
+                            <select name="category_add" id="category_add" class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
+                                <option value="Category One">Category One</option>
+                                <option value="Category Two">Category Two</option>
+                                <option value="Category Three">Category Three</option>
+                                <option value="Category Four">Category Four</option>
+                            </select>
+                        </div>
+                        <div>
+                            <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl border-none placeholder-gray-900 text-sm px-4 py-2" placeholder="Describe your idea"></textarea>
+                        </div>
+                        <div class="flex items-center justify-between space-x-3">
+                            <button
+                                type="button"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
+                            >
+                                <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                </svg>
+                                <span class="ml-1">Attach</span>
+                            </button>
+                            <button
+                                type="submit"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+                            >
+                                <span class="ml-1">Submit</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
             <div class="w-[800px]">

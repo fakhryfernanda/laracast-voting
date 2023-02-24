894b86dd00d4137e2d768cacfb37519d4d4891de
fakhryfernanda <mff023@gmail.com>
2023-02-24 21:50:46 +0700

    (+) add test for category

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -2,96 +2,102 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
+use App\Models\Category;
 use App\Models\Idea;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 class ShowIdeasTest extends TestCase
 {
     use RefreshDatabase;
-
     /** @test */
     public function list_of_ideas_shows_on_main_page()
     {
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
         $ideaOne = Idea::factory()->create([
             'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
             'description' => 'Description of my first idea',
         ]);
 
         $ideaTwo = Idea::factory()->create([
             'title' => 'My Second Idea',
+            'category_id' => $categoryTwo->id,
             'description' => 'Description of my second idea',
         ]);
 
         $response = $this->get(route('idea.index'));
-
         $response->assertSuccessful();
         $response->assertSee($ideaOne->title);
         $response->assertSee($ideaOne->description);
+        $response->assertSee($categoryOne->name);
         $response->assertSee($ideaTwo->title);
         $response->assertSee($ideaTwo->description);
+        $response->assertSee($categoryTwo->name);
     }
 
     /** @test */
     public function single_idea_shows_correctly_on_the_show_page()
     {
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
         $idea = Idea::factory()->create([
+            'category_id' => $categoryOne->id,
             'title' => 'My First Idea',
             'description' => 'Description of my first idea',
         ]);
-
         $response = $this->get(route('idea.show', $idea));
-
         $response->assertSuccessful();
         $response->assertSee($idea->title);
         $response->assertSee($idea->description);
+        $response->assertSee($categoryOne->name);
     }
 
     /** @test */
     public function ideas_pagination_works()
     {
-        Idea::factory(Idea::PAGINATION_COUNT + 1)->create();
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+
+        Idea::factory(Idea::PAGINATION_COUNT + 1)->create([
+            'category_id' => $categoryOne->id,
+        ]);
 
         $ideaOne = Idea::find(1);
         $ideaOne->title = 'My First Idea';
         $ideaOne->save();
-
         $ideaEleven = Idea::find(11);
         $ideaEleven->title = 'My Eleventh Idea';
         $ideaEleven->save();
-
         $response = $this->get('/');
-
         $response->assertSee($ideaOne->title);
         $response->assertDontSee($ideaEleven->title);
-
         $response = $this->get('/?page=2');
-
         $response->assertSee($ideaEleven->title);
         $response->assertDontSee($ideaOne->title);
     }
-
     /** @test */
     public function same_idea_title_different_slugs()
     {
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
         $ideaOne = Idea::factory()->create([
+            'category_id' => $categoryOne->id,
             'title' => 'My First Idea',
             'description' => 'Description for my first idea',
         ]);
 
         $ideaTwo = Idea::factory()->create([
+            'category_id' => $categoryOne->id,
             'title' => 'My First Idea',
             'description' => 'Another Description for my first idea',
         ]);
-
         $response = $this->get(route('idea.show', $ideaOne));
-
         $response->assertSuccessful();
         $this->assertTrue(request()->path() === 'ideas/my-first-idea');
-
         $response = $this->get(route('idea.show', $ideaTwo));
-
         $response->assertSuccessful();
         $this->assertTrue(request()->path() === 'ideas/my-first-idea-1');
     }

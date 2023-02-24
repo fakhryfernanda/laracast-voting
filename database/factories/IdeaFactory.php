fd82bc9820b8c4b9d48ccd2718ebe159df5cd3d9
fakhryfernanda <mff023@gmail.com>
2023-02-24 20:35:05 +0700

    (+) ideas

---
+++ database/factories/IdeaFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Idea>
+ */
+class IdeaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Idea::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => ucwords($this->faker->words(4, true)),
+            'description' => $this->faker->paragraph(5),
+        ];
+    }
+}

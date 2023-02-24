be9ee3fb8a31ad1ee286425c3419abf7689722e1
fakhryfernanda <mff023@gmail.com>
2023-02-24 22:02:24 +0700

    (+) add status

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Idea;
+use App\Models\Status;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
 
@@ -25,6 +26,12 @@ class DatabaseSeeder extends Seeder
         Category::factory()->create(['name' => 'Category 2']);
         Category::factory()->create(['name' => 'Category 3']);
         Category::factory()->create(['name' => 'Category 4']);
+
+        Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+        Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
+        Status::factory()->create(['name' => 'In Progress', 'classes' => 'bg-yellow text-white']);
+        Status::factory()->create(['name' => 'Implemented', 'classes' => 'bg-green text-white']);
+        Status::factory()->create(['name' => 'Closed', 'classes' => 'bg-red text-white']);
 
         Idea::factory(30)->create();
     }

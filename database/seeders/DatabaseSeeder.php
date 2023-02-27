01428ad1aa09453728e922e452e7683618c0e16a
fakhryfernanda <mff023@gmail.com>
2023-02-27 07:45:19 +0700

    (+) Vote

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Idea;
+use App\Models\User;
+use App\Models\Vote;
 use App\Models\Status;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
@@ -15,12 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Fakhry',
+            'email' => 'fakhryfernanda@gmail.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(19)->create();
 
         Category::factory()->create(['name' => 'Category 1']);
         Category::factory()->create(['name' => 'Category 2']);
@@ -33,6 +35,18 @@ class DatabaseSeeder extends Seeder
         Status::factory()->create(['name' => 'Implemented', 'classes' => 'bg-green text-white']);
         Status::factory()->create(['name' => 'Closed', 'classes' => 'bg-red text-white']);
 
-        Idea::factory(30)->create();
+        Idea::factory(100)->create();
+
+        // Generate unique votes. Ensure idea_id and user_id are unique for each row
+        foreach (range(1, 20) as $user_id) {
+            foreach (range(1, 100) as $idea_id) {
+                if ($idea_id % 2 === 0) {
+                    Vote::factory()->create([
+                        'user_id' => $user_id,
+                        'idea_id' => $idea_id,
+                    ]);
+                }
+            }
+        }
     }
 }

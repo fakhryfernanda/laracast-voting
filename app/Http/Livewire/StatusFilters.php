b3220c152de8344f18cd2925373bee5e3006c94f
fakhryfernanda <mff023@gmail.com>
2023-02-27 16:28:22 +0700

    (+) status filters count

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Status;
 use Livewire\Component;
 use Illuminate\Support\Facades\Route;
 
 class StatusFilters extends Component
 {
     public $status = 'All';
+    public $statusCount;
 
     protected $queryString = [
         'status',
@@ -15,6 +17,8 @@ class StatusFilters extends Component
 
     public function mount()
     {
+        $this->statusCount = Status::getCount();
+        
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
             $this->queryString = [];

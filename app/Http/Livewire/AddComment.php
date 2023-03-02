75ef969649ea287cad3c631906eb11a4f5f80e59
fakhryfernanda <mff023@gmail.com>
2023-03-02 13:46:48 +0700

    View Notifications

--- app/Http/Livewire/AddComment.php
+++ app/Http/Livewire/AddComment.php
@@ -6,6 +6,7 @@ use App\Models\Idea;
 use App\Models\Comment;
 use Livewire\Component;
 use Illuminate\Http\Response;
+use App\Notifications\CommentAdded;
 
 class AddComment extends Component
 {
@@ -28,7 +29,7 @@ class AddComment extends Component
 
         $this->validate();
 
-        Comment::create([
+        $newComment = Comment::create([
             'user_id' => auth()->id(),
             'idea_id' => $this->idea->id,
             'status_id' => 1,
@@ -36,6 +37,8 @@ class AddComment extends Component
         ]);
 
         $this->reset('comment');
+
+        $this->idea->user->notify(new CommentAdded($newComment));
 
         $this->emit('commentWasAdded', 'Comment was posted!');
     }

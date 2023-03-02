24fdab003cd1d4693ae7d8a2b844f0db7a14d482
fakhryfernanda <mff023@gmail.com>
2023-03-02 09:50:14 +0700

    Edit Comments

--- resources/views/components/notification-success.blade.php
+++ resources/views/components/notification-success.blade.php
@@ -35,6 +35,9 @@
             Livewire.on('commentWasAdded', message => {
                 showNotification(message)
             })
+            Livewire.on('commentWasUpdated', message => {
+                showNotification(message)
+            })
         @endif
     "
     x-show="isOpen"

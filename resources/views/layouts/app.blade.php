dfa50fe50b142945b3b85356879b1ea3b222c534
fakhryfernanda <mff023@gmail.com>
2023-03-02 14:01:25 +0700

    Mark Notifications as Read

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,6 +114,14 @@
             />
         @endif
 
+        @if (session('error_message'))
+            <x-notification-success
+                type="error"
+                :redirect="true"
+                message-to-display="{{ (session('error_message')) }}"
+            />
+        @endif
+
         <livewire:scripts />
     </body>
 </html>
